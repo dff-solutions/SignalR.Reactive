41cbaffaa0c7d5c8a244b63b557b5fa15cdd8fb0
Frank Koenigstein <frank.koenigstein@dff-solutions.de>
2018-03-06 16:04:17 +0100

    feat: owin startup

--- SignalR.Reactive/SignalR.Reactive/Configuration.cs
+++ SignalR.Reactive/SignalR.Reactive/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Owin;
 using Microsoft.Owin;
 
 [assembly: OwinStartup(typeof(SignalR.Reactive.Startup))]
@@ -8,36 +10,22 @@ namespace SignalR.Reactive
     {
         public void Configuration(IAppBuilder app)
         {
+            DependencyResolverContext.Instance = GlobalHost.DependencyResolver;
+
+            if (DependencyResolverContext.Instance == null)
+                throw new InvalidOperationException("DependenyResolver must be set to an instance of IDependencyResolver");
+
+            DependencyResolverContext.Instance.EnableRxSupport();
+
+            var config = new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            };
+
+            config.Resolver = DependencyResolverContext.Instance;
+
             // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            app.MapSignalR(config);
         }
     }
 }
-
-//[assembly: PreApplicationStartMethod(typeof(SignalR.Reactive.Configuration), "EnableRxSupport")]
-//namespace SignalR.Reactive
-//{
-//    public static class Configuration
-//    {
-//        public static void EnableRxSupport()
-//        {
-//            DependencyResolverContext.Instance = GlobalHost.DependencyResolver;
-            
-//            if (DependencyResolverContext.Instance == null)
-//                throw new InvalidOperationException("DependenyResolver must be set to an instance of IDependencyResolver");
-
-//            DependencyResolverContext.Instance.EnableRxSupport();
-//            //ToDo 
-//            var config = new HubConfiguration
-//                {
-//                    EnableDetailedErrors = true
-//                };
-
-//            config.Resolver = DependencyResolverContext.Instance;
-
-//            RouteTable.Routes.MapHubs(config); 
-//            //AspNetBootstrapper.Initialize();
-
-//        }
-//    }
-//}
